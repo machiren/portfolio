32aaed45f919ddf097ca2ab20a2a458831da7fe3
Ren Machida <machiren111@gmail.com>
2022-02-12 16:26:10 +0900

    add pathpida

---
+++ lib/$path.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+// prettier-ignore
+export const pagesPath = {
+  posts: {
+    _id: (id: string | number) => ({
+      $url: (url?: { hash?: string }) => ({ pathname: '/posts/[id]' as const, query: { id }, hash: url?.hash })
+    }),
+    $url: (url?: { hash?: string }) => ({ pathname: '/posts' as const, hash: url?.hash })
+  },
+  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash })
+}
+
+// prettier-ignore
+export type PagesPath = typeof pagesPath
